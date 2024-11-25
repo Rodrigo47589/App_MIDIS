af7749238a7e0d4cd20b8d3af92cf48dafd5216d
Rodrigo47589 <rodrigo.rodriguez.r@upch.pe>
2024-11-25 17:00:39 -0500

    Actualizacion parte siete

--- lib/Paginas/PreguntasVisual/visualfinal.dart
+++ lib/Paginas/PreguntasVisual/visualfinal.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Paginas/score_manager.dart';
 
 class QuestionScreenfinal extends StatelessWidget {
+  const QuestionScreenfinal({super.key});
+
   @override
   Widget build(BuildContext context) {
     final scoreManager = ScoreManager();
